9f70cc53f9cd484c51f249dbad1fb6745d3525fa
Maure Andrade <maure.andrade@escolar.ifrn.edu.br>
2022-09-28 21:56:22 -0300

    Questao 5

--- lista1/mips4.asm
+++ lista1/mips4.asm
@@ -1,20 +1,21 @@
 .text
 
-main: addi $v0, $zero, 5
-      syscall
-      
-      add $t0, $v0, $zero
-      
-      addi $v0, $zero, 5
-      syscall
-      add $t1, $v0, $zero
-      
-      add $t2, $t0, $t1
-      
-      addi $t3, $zero, 2
-      
-      div $t4, $t2, $t3
-      
-      add $a0, $t4, $zero
-      addi $v0, $zero, 1
-      syscall
+main: 
+	addi $v0, $zero, 5
+	syscall
+	
+	add $t0, $v0, $zero
+	
+	addi $v0, $zero, 5
+	syscall
+	
+	add $t0, $v0, $t0
+	
+	div $a0, $t0, 2
+	
+	addi $v0, $zero, 1
+	syscall
+
+	addi $v0, $zero, 10
+	syscall	
+	
